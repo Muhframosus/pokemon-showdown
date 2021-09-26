ee8ce79e9b5ba18cd5e1c5f86e3d0bc3b8db307f
Muhframos <90287802+Muhframosus@users.noreply.github.com>
2021-09-25 23:18:40 -0300

    ok..

--- config/formats.ts
+++ config/formats.ts
@@ -18,145 +18,18 @@ The column value will be ignored for repeat sections.
 */
 
 export const Formats: FormatList = [
+
+	// Sw/Sh Singles
+	///////////////////////////////////////////////////////////////////
+
 	{
 		section: "National Dex",
 	},
 	{
 		name: "[Gen 8] Adventures OU",
+		desc: `The standard format for Adventures.`,
 		mod: 'adventures',
 		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
-		banlist: [
-			'Uber', 'AG', 'Baton Pass',
-		],
-	},
-	{
-		name: "[Gen 8] Adventures UU",
-
-		mod: 'adventures',
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
-		banlist: [
-			// National Dex OU
-			 'OU', 'AG', 'UUBL', 'Uber', 'Drought', 'Drizzle', 'Arena Trap', 'Shadow Tag', 'Baton Pass',
-			// Slowbronite is banned so it doesn't validate on Galarian Slowbro
-			'Slowbronite',
-		],
-	},
-	{
-		name: "[Gen 8] Adventures RU",
-
-		mod: 'adventures',
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
-		banlist: [
-			// National Dex OU
-			 'OU', 'UU', 'UUBL', 'AG', 'Uber', 'Drought', 'Drizzle', 'Arena Trap', 'Shadow Tag', 'Baton Pass',
-			// Slowbronite is banned so it doesn't validate on Galarian Slowbro
-			'Slowbronite',
-		],
-	},
-	{
-		name: "[Gen 8] Adventures NU",
-
-		mod: 'adventures',
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
-		banlist: [
-			// National Dex OU
-			 'OU', 'UU', 'RU', 'AG', 'UUBL', 'Uber', 'Drought', 'Drizzle', 'Arena Trap', 'Shadow Tag', 'Baton Pass',
-			// Slowbronite is banned so it doesn't validate on Galarian Slowbro
-			'Slowbronite',
-		],
-	},
-	{
-		name: "[Gen 8] Adventures DOU",
-		mod: 'adventures',
-		gameType: 'doubles',
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
-		banlist: [
-			'Uber', 'AG', 'Baton Pass',
-		],
-	},
-	{
-		name: "[Gen 8] Adventures DUU",
-		mod: 'adventures',
-		gameType: 'doubles',
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
-		banlist: [
-			// National Dex OU
-			 'DOU', 'AG', 'Uber', 'Drought', 'Drizzle', 'Arena Trap', 'Shadow Tag', 'Baton Pass',
-			, // National Dex UUBL
-			// Slowbronite is banned so it doesn't validate on Galarian Slowbro
-			'Slowbronite',
-		],
-		// Used to distinguish UU from below UU in the client
-	},
-	{
-		name: "[Gen 8] Adventures Ubers",
-		mod: 'adventures',
-		ruleset: ['Standard NatDex', 'Dynamax Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Sleep Clause Mod',],
-		banlist: ['AG', 'Baton Pass',],
-	},
-	{
-		name: "[Gen 8] Adventures AG",
-		mod: 'adventures',
-		ruleset: ['Standard NatDex'],
-	},
-	{
-		name: "[Gen 8] Adventures 1v1",
-		desc: `Bring three Pok&eacute;mon to Team Preview and choose one to battle.`,
-		mod: 'adventures',
-		teamLength: {
-			validate: [1, 3],
-			battle: 1,
-		},
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
-		banlist: [
-			'Calyrex-Ice', 'Calyrex-Shadow', 'Cinderace', 'Dialga', 'Dragonite', 'Eternatus', 'Giratina', 'Giratina-Origin', 'Groudon', 'Ho-Oh', 'Kyogre', 'Kyurem-Black',
-			'Kyurem-White', 'Lugia', 'Lunala', 'Magearna', 'Marshadow', 'Melmetal', 'Mew', 'Mewtwo', 'Mimikyu', 'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane', 'Palkia',
-			'Rayquaza', 'Reshiram', 'Sableye', 'Solgaleo', 'Victini', 'Xerneas', 'Yveltal', 'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom',
-			'Moody', 'Focus Sash', 'Perish Song', 'Eternatus-Eternamax', 'AG', 'Uber',
-		],
-	},
-	{
-		name: "[Gen 8] Adventures Free For All",
-
-		mod: 'adventures',
-		gameType: 'freeforall',
-		challengeShow: false,
-		rated: false,
-		banlist: ['Eternatus-Eternamax', 'AG',],
-		ruleset: [ 'Species Clause', 'Dynamax Clause', 'Obtainable Good', '+Unobtainable', 'HP Percentage Mod', 'Sleep Clause Mod', '+Past', 'Nickname Clause', 'Endless Battle Clause', 'Cancel Mod',],
-	},
-	{
-		name: "[Gen 8] Adventures Multi Battle",
-
-		mod: 'adventures',
-		challengeShow: false,
-		gameType: 'multi',
-		teamLength: {
-			battle: 3,
-	},
-		banlist: [ 'Eternatus-Eternamax', 'AG',],
-		ruleset: ['Species Clause', 'Dynamax Clause', 'Obtainable Good', '+Unobtainable', 'HP Percentage Mod', 'Sleep Clause Mod', '+Past', 'Nickname Clause', 'Endless Battle Clause', 'Cancel Mod',],
-	},
-	{
-		name: "[Gen 8] Adventures Custom Game",
-		mod: 'adventures',
-		rated: false,
-		maxLevel: 9999,
-		battle: {trunc: Math.trunc},
-		defaultLevel: 100,
-		teamLength: {
-			validate: [1, 24],
-			battle: 24,
-		},
-		// no restrictions, for serious (other than team preview)
-		ruleset: ['Team Preview', 'Cancel Mod'],
-	},
-	{
-		name: "[Gen 8] Adventures FFA Custom Game",
-
-		mod: 'adventures',
-		gameType: 'freeforall',
-		rated: false,
-		ruleset: ['Team Preview', 'Cancel Mod'],
+		banlist: [ 'Uber', 'AG', 'Baton Pass', ],
 	},
 ];
