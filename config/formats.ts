9aa3edf8b86426c55b09fa7f1309d44f759ec214
Muhframos <90287802+Muhframosus@users.noreply.github.com>
2021-09-25 23:13:11 -0300

    hnf

--- config/formats.ts
+++ config/formats.ts
@@ -159,49 +159,4 @@ export const Formats: FormatList = [
 		rated: false,
 		ruleset: ['Team Preview', 'Cancel Mod'],
 	},
-	//Axis League
-	{
-		section: "Axis League Dex",
-	},
-	{
-		name: "[Gen 8] Axis League Custom Game",
-
-		mod: 'torchic',
-		searchShow: false,
-		debug: true,
-		maxLevel: 9999,
-		battle: {trunc: Math.trunc},
-		defaultLevel: 100,
-		teamLength: {
-			validate: [1, 24],
-			battle: 24,
-		},
-		// no restrictions, for serious (other than team preview)
-		ruleset: ['Team Preview', 'Cancel Mod'],
-	},
-	{
-		name: "[Gen 8] Axis League AG",
-		mod: 'torchic',
-		ruleset: ['Standard NatDex', 'Dynamax Clause'],
-	},
-	{
-		name: "[Gen 8] Axis League Elite 4 (Ubers)",
-		mod: 'torchic',
-		ruleset: ['Standard NatDex', 'Dynamax Clause',],
-		banlist: [ 'AG', 'Baton Pass'],
-	},
-	{
-		name: "[Gen 8] Axis League Gyms (OU)",
-		mod: 'torchic',
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
-		banlist: [
-			'Alakazam-Mega', 'Arceus', 'Blastoise-Mega', 'Blaziken-Mega', 'Calyrex-Ice', 'Calyrex-Shadow', 'Darkrai', 'Darmanitan-Galar', 'Deoxys-Attack',
-			'Deoxys-Base', 'Deoxys-Speed', 'Dialga', 'Dracovish', 'Dragapult', 'Eternatus', 'Genesect', 'Gengar-Mega', 'Giratina', 'Giratina-Origin',
-			'Groudon', 'Ho-Oh', 'Kangaskhan-Mega', 'Kyogre', 'Kyurem-Black', 'Kyurem-White', 'Landorus-Base', 'Lucario-Mega', 'Lugia', 'Lunala',
-			'Marshadow', 'Metagross-Mega', 'Mewtwo', 'Naganadel', 'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane', 'Necrozma-Ultra', 'Palkia', 'Pheromosa',
-			'Rayquaza', 'Reshiram', 'Salamence-Mega', 'Shaymin-Sky', 'Solgaleo', 'Spectrier', 'Tornadus-Therian', 'Urshifu-Base', 'Xerneas', 'Yveltal',
-			'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom', 'Zygarde-Base', 'Zygarde-Complete',
-			 'Moody', 'Power Construct', 'Shadow Tag', 'Baton Pass',
-		],
-	},
 ];
