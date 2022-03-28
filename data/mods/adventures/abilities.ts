2025541f487ec0857dd074a094f1db31903fdbf7
Meq <meq8598@gmail.com>
2022-03-28 17:37:53 -0400

    Update abilities.ts

--- data/mods/adventures/abilities.ts
+++ data/mods/adventures/abilities.ts
@@ -1715,8 +1715,8 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		name: "Teravolt",
 		rating: 5,
 		num: 163,
- },
- FireSpread: {
+},
+FireSpread: {
            onModifyTypePriority: +1,
         onModifyType(move, pokemon) {
             const noModifyType = [
@@ -1725,5 +1725,5 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
             if (move.type === 'Normal' && !noModifyType.includes(move.id) && !(move.isZ && move.category !== 'Status')) {
                 move.type = 'Fire';
                 move.aerilateBoosted = true;
-	},
-};
+},
+
