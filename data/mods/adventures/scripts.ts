a254265a3943cd08f2e361bb67501fefa6fc4707
FierceFlame6098 <hegdeankit7@gmail.com>
2022-04-05 21:14:04 -0400

    Update scripts.ts

--- data/mods/adventures/scripts.ts
+++ data/mods/adventures/scripts.ts
@@ -1619,6 +1619,14 @@ this.modData('Learnsets', 'gastrodon').learnset.spikes = ['8L1'];
 
 this.modData('Learnsets', 'suicune').learnset.flipturn = ['8L1'];  
 
+this.modData('Learnsets', 'gourgeistsmall').learnset.flamethrower = ['8L1']; 
+
+this.modData('Learnsets', 'gourgeistlarge').learnset.flamethrower = ['8L1']; 
+
+this.modData('Learnsets', 'gourgeist').learnset.flareblitz = ['8L1']; 
+
+this.modData('Learnsets', 'gourgeistsuper').learnset.flareblitz = ['8L1']; 
+
 
 //NEW MOVES END-
 
