131a2f46fab270f91165ed6af331e20861ec9404
FierceFlame6098 <hegdeankit7@gmail.com>
2022-04-15 09:10:12 -0400

    Update pokedex.ts

--- data/mods/adventures/pokedex.ts
+++ data/mods/adventures/pokedex.ts
@@ -1,1 +1,11 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+
+     palossand: {
+		inherit: true,
+		baseStats: {hp: 95, atk: 100, def: 135, spa: 100, spd: 85, spe: 20},
+		abilities: {0: "Water Compaction", H: "Power Compaction", 1: "Sand Stream", },
+		otherFormes: ["Palossand-Mega"],
+		formeOrder: ["Palossand", "Palossand-Mega"],
+
+      },
+      }
