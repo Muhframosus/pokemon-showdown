2e12758d5d033596eb8d2adeafb570d395cdd537
Muhframos <90287802+Muhframosus@users.noreply.github.com>
2022-06-29 19:17:01 -0300

    DOUBLE FORMAT MOMENT

--- config/formats.ts
+++ config/formats.ts
@@ -164,4 +164,14 @@ export const Formats: FormatList = [
 		// no restrictions, for serious (other than team preview)
 		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
 	},
+	{
+		name: "[Gen 8] Fierce Format Custom Game",
+
+		mod: 'fierceformat',
+		searchShow: false,
+		debug: true,
+		battle: {trunc: Math.trunc},
+		// no restrictions, for serious (other than team preview)
+		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
+	},
 ];
