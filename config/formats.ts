75dc21978e5aa9603deab2a288e847dee7f0718a
Bennoloth <78837399+Bennoloth@users.noreply.github.com>
2022-07-04 20:08:10 -0400

    FF Typo

--- config/formats.ts
+++ config/formats.ts
@@ -164,7 +164,7 @@ export const Formats: FormatList = [
 		// no restrictions, for serious (other than team preview)
 		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
 	},
-	
+	{
 		name: "[Gen 8] Fierce Format",
 		desc: `The format run by Fierce Flamethrower.`,
 		mod: 'fierceformat',
