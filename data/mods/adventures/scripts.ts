9247cbc91829ecf5ad74a329f7767e2aa98a9b7f
Bennoloth <78837399+Bennoloth@users.noreply.github.com>
2022-07-07 23:37:16 -0400

    Metal Powder

--- data/mods/adventures/scripts.ts
+++ data/mods/adventures/scripts.ts
@@ -669,8 +669,6 @@ this.modData('Learnsets', 'milotic').learnset.charm = ['8L1'];
 this.modData('Learnsets', 'milotic').learnset.babydolleyes = ['8L1'];
 this.modData('Learnsets', 'milotic').learnset.dragonrage = ['8L1'];
 
-this.modData('Learnsets', 'corviknight').learnset.beakblast = ['8L1'];
-
 this.modData('Learnsets', 'falinks').learnset.defendorder = ['8L1'];
 this.modData('Learnsets', 'falinks').learnset.attackorder = ['8L1'];
 this.modData('Learnsets', 'falinks').learnset.barrage = ['8L1'];
