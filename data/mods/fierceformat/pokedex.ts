7105e7c652795617d9597edde74bac20faf15bd8
Scywave <103772313+Scywave@users.noreply.github.com>
2022-07-09 21:17:50 -0400

    Update pokedex.ts

--- data/mods/fierceformat/pokedex.ts
+++ data/mods/fierceformat/pokedex.ts
@@ -4,9 +4,5 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		inherit: true,
 		baseStats: {hp: 98, atk: 100, def: 135, spa: 100, spd: 85, spe: 20},
 		abilities: {0: "Water Compaction", H: "Power Compaction", 1: "Sand Stream", },
-		otherFormes: ["Palossand-Mega"],
-		formeOrder: ["Palossand", "Palossand-Mega"],
-
-
       },	
       }	
