e9957805bb1f2538ca2398b649a6d87071eca60d
Bennoloth <78837399+Bennoloth@users.noreply.github.com>
2022-07-12 22:52:29 -0400

    Licky fixy

--- data/mods/adventures/conditions.ts
+++ data/mods/adventures/conditions.ts
@@ -61,12 +61,40 @@ export const Conditions: {[k: string]: ConditionData} = {
 	},
 	lickytrapped: {
 		name: 'lickytrapped',
-		duration: 2,
 		onBeforeMovePriority: 4,
 		onBeforeMove(pokemon) {
 			this.add('cant', pokemon, 'lickytrapped');
 			return false;
 		},
+		duration: 5,
+		durationCallback(target, source) {
+			if (source?.hasItem('gripclaw')) return 8;
+			return this.random(5, 7);
+		},
+		onStart(pokemon, source) {
+			this.add('-activate', pokemon, 'move: ' + this.effectState.sourceEffect, '[of] ' + source);
+			this.effectState.boundDivisor = source.hasItem('bindingband') ? 6 : 8;
+		},
+		onResidualOrder: 13,
+		onResidual(pokemon) {
+			const source = this.effectState.source;
+			// G-Max Centiferno and G-Max Sandblast continue even after the user leaves the field
+			const gmaxEffect = ['gmaxcentiferno', 'gmaxsandblast'].includes(this.effectState.sourceEffect.id);
+			if (source && (!source.isActive || source.hp <= 0 || !source.activeTurns) && !gmaxEffect) {
+				delete pokemon.volatiles['partiallytrapped'];
+				this.add('-end', pokemon, this.effectState.sourceEffect, '[partiallytrapped]', '[silent]');
+				return;
+			}
+			this.damage(pokemon.baseMaxhp / this.effectState.boundDivisor);
+		},
+		onEnd(pokemon) {
+			this.add('-end', pokemon, this.effectState.sourceEffect, '[partiallytrapped]');
+		},
+		onTrapPokemon(pokemon) {
+			const gmaxEffect = ['gmaxcentiferno', 'gmaxsandblast'].includes(this.effectState.sourceEffect.id);
+			if (this.effectState.source?.isActive || gmaxEffect) pokemon.tryTrap();
+		},
+	},
 	},
 	lickytrappinglock: {
 		name: 'lickytrappinglock',
