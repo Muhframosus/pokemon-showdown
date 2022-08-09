547c95e4eee09c66ea9920fb813faafaae1c6fab
Bennoloth <78837399+Bennoloth@users.noreply.github.com>
2022-08-09 11:16:18 -0400

    Metal Burst

--- config/formats.ts
+++ config/formats.ts
@@ -136,7 +136,6 @@ export const Formats: FormatList = [
 		name: "[Gen 8] Adventures Custom Game",
 
 		mod: 'adventures',
-		debug: true,
 		rated: false,
 		battle: {trunc: Math.trunc},
 		// no restrictions, for serious (other than team preview)
