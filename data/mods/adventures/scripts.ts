40b0a2d001887dc8c15ae2d8371c4233ab82a8ff
Bennoloth <78837399+Bennoloth@users.noreply.github.com>
2022-08-15 22:55:18 -0400

    Basculegion test 1

--- data/mods/adventures/scripts.ts
+++ data/mods/adventures/scripts.ts
@@ -1110,6 +1110,8 @@ this.modData('Learnsets', 'carnivine').learnset.snaptrap = ['8L1'];
 
 this.modData('Learnsets', 'eeveestarter').learnset.storedpower = ['8L1'];
 
+this.modData('Learnsets', 'basculegion').learnset.storedpower = ['8L1'];
+
 this.modData('Learnsets', 'grapploct').learnset.glare = ['8L1'];
 
 this.modData('Learnsets', 'forretress').learnset.topsyturvy = ['8L1'];
